7bb3f948eddb7b45f74efb33aed0f89789745ccc
AlanPintor <jaime.pintortejeda@georgebrown.ca>
2020-11-08 18:51:24 -0500

    Login error message working

--- src/main/java/frontend/LoginController.java
+++ src/main/java/frontend/LoginController.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,11 +49,17 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView login(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password) {
+    public ModelAndView login(@RequestParam(value = "email") String email,
+                              @RequestParam(value = "password") String password,
+                              Model model) {
         User user = userRepository.findByEmail(email);
-        if (Objects.nonNull(user) && email.equals(user.getEmail()) && password.equals(user.getPassword())) {
+        if (Objects.nonNull(user) && email.equals(user.getEmail())
+                && password.equals(user.getPassword())) {
             return new ModelAndView(new RedirectView("/dashboard", true));
         } else {
+            // To display login error message
+            model.addAttribute("invalidCredentials", true);
+
             return new ModelAndView("login");
         }
     }
